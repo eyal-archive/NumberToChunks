128ab13953e030c188b65bc81e37d312266e5698
Eyal Shilony <git@shilony.net>
2014-08-05 04:49:17 +0300

    Initial commit.

---
+++ NumberToChunks/NumberToChunks.cs
@@ -0,0 +1,32 @@
+﻿namespace NumberToChunks
+{
+	using System.Collections.Generic;
+	using System.Globalization;
+
+	public static class NumberToChunks
+	{
+		public static IEnumerable<int> GetChunks(long number, int chunkSize)
+		{
+			string set = number.ToString(CultureInfo.InvariantCulture);
+
+			int i = 0;
+
+			while (i < set.Length)
+			{
+				i += chunkSize;
+
+				int position = set.Length - i;
+
+				bool isNegative = position < 0;
+
+				int index = isNegative ? 0 : position;
+
+				int length = isNegative ? chunkSize + position : chunkSize;
+
+				int chunk = int.Parse(set.Substring(index, length));
+
+				yield return chunk;
+			}
+		}
+	}
+}
---
+++ NumberToChunks/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NumberToChunks
+{
+	using System;
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			const long number = 132423123453 * 2;
+
+			foreach (var chunk in NumberToChunks.GetChunks(number, 1))
+			{
+				Console.WriteLine(chunk);
+			}
+
+			Console.ReadKey(false);
+		}
+	}
+}
